9ac4404784fb754a2df2fa8739d8cce4a139dcda
StardustDL <stardustdl@163.com>
2021-07-26 11:31:00 +0800

    support fluent api for prompter configer

--- src/schemdule/prompters/configer.py
+++ src/schemdule/prompters/configer.py
@@ -10,7 +10,7 @@ class PrompterConfiger:
     def __init__(self) -> None:
         self._result: Optional[Prompter] = None
 
-    def use(self, prompter: Prompter) -> None:
+    def use(self, prompter: Prompter) -> "PrompterConfiger":
         if self._result is None:
             self._result = prompter
         elif isinstance(self._result, PrompterHub):
@@ -18,16 +18,17 @@ class PrompterConfiger:
         else:
             raise Exception(
                 f"The prompter is set to a no-hub prompter {type(self._result)}.")
+        return self
 
-    def useBroadcaster(self) -> None: self.use(PrompterBroadcaster())
+    def useBroadcaster(self) -> "PrompterConfiger": return self.use(PrompterBroadcaster())
 
-    def useSwitcher(self) -> None: self.use(PrompterSwitcher())
+    def useSwitcher(self) -> "PrompterConfiger": return self.use(PrompterSwitcher())
 
-    def useConsole(self) -> None: self.use(ConsolePrompter())
+    def useConsole(self) -> "PrompterConfiger": return self.use(ConsolePrompter())
 
-    def useTkinterMessageBox(self) -> None: self.use(TkinterMessageBoxPrompter())
+    def useTkinterMessageBox(self) -> "PrompterConfiger": return self.use(TkinterMessageBoxPrompter())
 
-    def useMessageBox(self, auto_close=False) -> None: self.use(MessageBoxPrompter(auto_close))
+    def useMessageBox(self, auto_close=False) -> "PrompterConfiger": return self.use(MessageBoxPrompter(auto_close))
 
     def build(self) -> Prompter:
         return self._result
